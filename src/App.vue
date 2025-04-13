806955ebb7a26efd340383889630f77450d6e87e
Rémi Emonet <remi.emonet@reverse--com.heeere>
2025-04-13 18:51:15 +0200

    Types and user setup

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,25 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
+import { UserCog } from '@vicons/fa'
+import { Icon } from '@vicons/utils'
+import { useLocalStore } from './stores/simple'
+
+const local = useLocalStore()
+
+function changeUserName() {
+  const name = prompt('Enter your name:', local.userName)
+  if (name) {
+    local.userName = name
+  }
+}
 </script>
 
 <template>
   <header>
+    <div @click="changeUserName()">
+      <Icon><UserCog/></Icon>
+      <span class="username">{{ local.userName }}</span>
+    </div>
     <!--
       <nav>
         <RouterLink to="/">Home</RouterLink>
--- src/typing.ts
+++ src/typing.ts
@@ -1,3 +1,27 @@
-export type Data = {
-  count: number
-}
+
+import { z } from 'zod'
+
+export const zPerson = z.string()
+export type Person = z.infer<typeof zPerson>
+
+export const zDate = z.string()
+export type Date = z.infer<typeof zDate>
+
+export const zTime = z.number().int()
+export type Time = z.infer<typeof zTime>
+
+export const zActivity = z.string()
+export type Activity = z.infer<typeof zActivity>
+
+export const zLog = z.object({
+  at: zTime,
+  activity: zActivity,
+  quantity: z.number(),
+})
+
+export const zData = z.object({
+  count: z.number(),
+  logs: z.record(zPerson, z.record(zDate, zLog.array())),
+})
+export type Data = z.infer<typeof zData>
+
