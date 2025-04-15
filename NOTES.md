6972c9e071abbc658e23972e5d5c10458ea1240e
Rémi Emonet <remi.emonet@reverse--com.heeere>
2025-04-15 18:51:43 +0200

    Handle offline (pwa)

--- NOTES.md
+++ NOTES.md
@@ -7,6 +7,7 @@ pnpm  build --base /2025-04-renfollow/
 
 TODO
 
-- add offline support, clean
+- add possibility to add comment (a special type of activity?)
+- integrate offline into either #yjs or #yjsoffline or #offline + #yjsoffline (merge of offline and yjs)
 
 
--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -11,6 +12,20 @@ export default defineConfig({
     vue(),
     vueJsx(),
     vueDevTools(),
+    VitePWA({
+      registerType: 'prompt',
+      injectRegister: 'auto',
+      pwaAssets: {
+        disabled: false,
+        config: true,
+      },
+      manifest: {
+        name: 'Renfollow',
+        short_name: 'renfollow',
+        description: 'Follow you renfo',
+        theme_color: '#0000ff',
+      },
+    }),
   ],
   resolve: {
     alias: {
