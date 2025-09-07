f8fdc0897697188dccd9d995bba644a043a443e3
Rémi Emonet <remi.emonet@reverse--com.heeere>
2025-09-07 12:30:30 +0200

    Update to setupYjsAsk

--- src/main.ts
+++ src/main.ts
@@ -5,15 +5,15 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
-import { setupYjs } from 'yjs-datatools'
 import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
+import { setupYjsAsk } from 'yjs-datatools/ysetup'
 
 const app = createApp(App)
 const pinia = createPinia()
 
 pinia.use(piniaPluginPersistedstate)
 export const APP_LOCAL_STORAGE_KEY = 'yap-habits:server'
-export const yjs = await setupYjs({ lsKey: APP_LOCAL_STORAGE_KEY })
+export const yjs = await setupYjsAsk(APP_LOCAL_STORAGE_KEY)
 
 app.use(pinia)
 app.use(router)
